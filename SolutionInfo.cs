1d875f02b896c497220fb82d74c9bfd1753b9fab
Harald Tochenhagen <harald.tochenhagen@schoenhofer.de>
2018-09-13 09:58:12 +0200

    Add "OneClickBuild" to solution

---
+++ SolutionInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("Caliburn.Micro.MessageNavigator")]
+
+[assembly: AssemblyDescription(@"Displays all publications and handlers of 'Caliburn.Micro' message types treated according to the search performed")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en", UltimateResourceFallbackLocation.MainAssembly)]
